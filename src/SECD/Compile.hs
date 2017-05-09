5af568865ed48b2913647238a8bcb701f657ee7d
Rui Andrade <ruiandrade@outlook.com>
2017-05-09 01:42:00 +0100

    fixing compilation error in recursive and standard functions

--- src/SECD/Compile.hs
+++ src/SECD/Compile.hs
@@ -15,8 +15,10 @@ compile (If e0 e1 e2) sym = (compile e0 sym) ++ [SEL c1 c2]
     where 
         c1 = compile e1 sym ++ [JOIN]
         c2 = compile e2 sym ++ [JOIN]
-compile (Lam x e) sym = [LDF (compile e s1)] ++ [RTN]
+compile (Lam x e) sym = [LDF ((compile e s1) ++ [RTN])]
     where s1 = extend x sym  -- extend serve para acrescentar o x ao sym, vai buscar a posicao next na memoria
 compile (App e1 e2) sym = (compile e1 sym) ++ (compile e2 sym) ++ [AP]
-compile (Fix (Lam f (Lam x e))) sym = [LDRF (compile e s1)] ++ [RTN]
+compile (Fix (Lam f (Lam x e))) sym = [LDRF ((compile e s1) ++ [RTN])]
     where s1 = extend x (extend f sym)
+
+compileProgram p = compile p [] ++ [HALT] -- compile a program and add HALT inst to the end
